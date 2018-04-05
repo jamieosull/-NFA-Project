01b3fc7ee8618a42abd9e4674f9e8084f47cddb1
jamieosull <g00340757@gmit.ie>
2018-04-05 19:52:35 +0100

    Update main.go

--- main.go
+++ main.go
@@ -14,6 +14,44 @@ func intopost(infix string) string {
 
 	//Stack stores operators from the infix regular expression
 	s := []rune{}
+	
+	//Loop over the input
+	//range(convert into array of runes using UTF)
+	for _, r := range infix {
+		switch {
+
+		case r == '(':
+			//puts open bracket at the end of the stack
+			s = append(s, r)
+		case r == ')':
+			//Pop of the stack until an opening bracket is found
+			//len(s)-1 = the last element on the stack
+			for s[len(s)-1] != '(' {
+				pofix = append(pofix, s[len(s)-1]) //last element of 's'
+				s = s[:len(s)-1]                   //everything in 's' except the last character
+			}
+
+			s = s[:len(s)-1]
+		//if a special character is found
+		case specials[r] > 0:
+			//while the stack still has an element on it and the precedence of the current character that reads is <= the precedence of the top element of the stack
+			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]] {
+				//Takes character of the stack and sticks into pofix
+				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+			}
+			
+			s = append(s, r)
+		
+		default:
+			//Takes the default characters and sticks it at the end of pofix
+			pofix = append(pofix, r)
+		}
+	}
+
+	//If there is anything left on the stack, append to pofix
+	for len(s) > 0 {
+		pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+	}
 
 
 	return string(pofix)
